932b7068025ec9853c36d799fdd383f2e887d0a7
john <johnzengshi@gmail.com>
2026-04-24 17:16:08 +0800

    Rename Apple and desktop app identifiers
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "im_flutter_uikit_full_platform");
+    gtk_header_bar_set_title(header_bar, "Tencent Cloud Chat Flutter");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "im_flutter_uikit_full_platform");
+    gtk_window_set_title(window, "Tencent Cloud Chat Flutter");
   }
 
   auto bdw = bitsdojo_window_from(window);
