73e79943a26625a92078ec7c366649ae97da07a6
john <johnzengshi@gmail.com>
2026-04-27 12:39:01 +0800

    Add Android startup classes under app package
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ android/app/src/main/kotlin/com/tencent/cloud/chat/flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tencent.cloud.chat.flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/tencent/cloud/chat/flutter/application/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.tencent.cloud.chat.flutter.application
+
+import com.tencent.chat.flutter.push.tencent_cloud_chat_push.application.TencentCloudChatPushApplication
+
+class MyApplication : TencentCloudChatPushApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
